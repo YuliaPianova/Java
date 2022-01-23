d0d2846824fc1a2fd78abfee78782e16edccd0ff
Yulia Pianova <91152939+YuliaPianova@users.noreply.github.com>
2022-01-23 15:35:31 -0800

    Add files via upload

---
+++ TestBat.java
@@ -0,0 +1,18 @@
+public class TestBat{
+    public static void main(String[] args){
+        Bat bat = new Bat(100);
+
+        bat.attackTown();
+        bat.attackTown();
+        bat.attackTown();
+        bat.eatHuman();
+        bat.eatHuman();
+        bat.fly();
+        bat.fly();
+        bat.displayEnergy();
+            
+        
+    }
+}
+
+        
